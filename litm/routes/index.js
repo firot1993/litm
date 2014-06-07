ba46925fddb419d83065072af1c60394d0e3e306
Onetothree <firot1993@gmail.com>
2014-06-07 14:53:08 +0800

    css

--- litm/routes/index.js
+++ litm/routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Litm' });
   // req.session.retry=0
   // res.redirect('/users')
 };
